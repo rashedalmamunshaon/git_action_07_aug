1308058c99b69b2a22ab24365347ac7861b729ba
rashedalmamunshaon <amd.mamun2030.gmail.com>
2025-08-08 00:04:01 +0600

    2nd  commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 def say_hello():
     print("Hello from GitHub Actions!")
+    print("This is a simple Python script running in a GitHub Actions workflow.")
 
 if __name__ == "__main__":
     say_hello()
